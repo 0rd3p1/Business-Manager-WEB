1f43d1eee8eb065546665c7161c84f9efca2b9cf
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2025-04-25 19:40:42 -0300

    Update

--- Projeto/views/404.view.php
+++
@@ -1,4 +0,0 @@
-<div class="flex flex-col place-items-center mt-80">
-    <h1 class="font-extrabold text-xl">Pagina não encontrada!</h1>
-    <h1 class="text-6xl">Error 404</h1>
-</div>
--- aula20/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function db () {
-    return $db = new PDO('sqlite:db.sqlite');
-}
-
-?>
