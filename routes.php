a1cd0ca2936fd176e7e67439bb37e32972d03ef2
Pedro Henrique Fernandes de Oliveira <fernandesdeoliveira261@gmail.com>
2025-06-03 19:30:09 -0300

    Route Att

--- routes.php
+++ routes.php
@@ -4,6 +4,8 @@ $route = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 
 if (!$route) $route = 'index';
 
+if ($route == 'index' && isset($_SESSION['idUser'])) $route = 'business';
+
 if (!file_exists("controllers/{$route}.controller.php")) {
     abort(404);
 }
